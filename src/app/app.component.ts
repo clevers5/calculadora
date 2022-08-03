2c201943b4e95bbcb849509e6163a638a3228beb
Clever <arnez.4292@gmail.com>
2022-08-03 00:01:50 -0400

    Add operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculadora';
+export class AppComponent implements OnInit {
+  titulo = 'calculadora';
+  screen="";
+
+  n1:any;
+  n2:any;
+  n3:any;
+  n4="";
+  n5="";
+
+  ngOnInit(): void{
+
+  }
+
+  //Calculadora
+
+  enterValue(value:string){
+    if((this.n2 == "+") || (this.n2 == "*" || (this.n2 == "/"))){
+      this.n4 = this.n4 + value;
+      this.screen = this.screen + value;
+      this.n3 = this.n4;
+    }else{
+      this.screen = this.screen + value;
+      this.n1 = this.screen;
+    }
+  }
+
+  condition(value: string){
+    
+    this.screen = this.screen + value;
+    this.n2 = value;
+  }
+  
+  clear(){
+    this.screen = "";
+    this.n1 = "";
+    this.n2 = "";
+    this.n3 = "";
+    this.n4 = "";
+  }
+
+  result(){
+    if (this.n2 == '+') {
+      this.screen = `${this.screen} = ${(parseInt(this.n1) + parseInt(this.n3)).toString()} `;
+      this.screen = (parseInt(this.screen) + parseInt(this.n3)).toString();
+    }
+
+    if (this.n2 == '-') {
+      this.screen = `${this.screen} = ${(parseInt(this.n1) - parseInt(this.n3)).toString()} `;
+      this.screen = (parseInt(this.screen) - parseInt(this.n3)).toString();
+    }
+
+    if (this.n2 == '*') {
+      this.screen = `${this.screen} = ${(parseInt(this.n1) * parseInt(this.n3)).toString()} `;
+      this.screen = (parseInt(this.screen) * parseInt(this.n3)).toString();
+    }
+
+    if (this.n2 == '/') {
+      this.screen = `${this.screen} = ${(parseInt(this.n1) / parseInt(this.n3)).toString()} `;
+      this.screen = (parseInt(this.screen) / parseInt(this.n3)).toString();
+    }
+
+
+  
+  }
+
 }
